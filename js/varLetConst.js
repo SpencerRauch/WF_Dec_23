8edab912bda8f2cf485f8259100c3dc65efd6006
Spencer Rauch <rauchs@gmail.com>
2024-05-02 14:54:34 -0700

    notes

--- js/varLetConst.js
+++ js/varLetConst.js
@@ -1,42 +1,42 @@
-// // const -- unchangeable
-// // let -- changeable
+// const -- unchangeable
+// let -- changeable
 
-// //scope
+//scope
 
-// //var is function scoped -- the context in which the variable will work (or is defined)
+//var is function scoped -- the context in which the variable will work (or is defined)
 
-// var x = "value"
+var x = "value"
 
-// if (true){
-//     var x = "new value"
-// }
+if (true){
+    var x = "new value"
+}
 
-// function myFunction(){
-//     var x = "even newer value"
-// }
-
-// // console.log(x)
-
-// //let and const are BLOCK scoped {}
-// let ourLet = "original"
-
-// {
-//     let ourLet = "block scoped version"
-// }
-
-// // let ourLet = "new value"
-
-// console.log(ourLet)
-//hoisting vars are hoisted and initialized, let and const are being hoisted 
+function myFunction(){
+    var x = "even newer value"
+}
 
 // console.log(x)
-// var x = "test value"
 
-// console.log(x)
-// let x = "value"
+//let and const are BLOCK scoped {}
+let ourLet = "original"
 
-// const myVal = 8;
-// myVal = 9;
+{
+    let ourLet = "block scoped version"
+}
+
+// let ourLet = "new value"
+
+console.log(ourLet)
+// hoisting vars are hoisted and initialized, let and const are being hoisted 
+
+console.log(x)
+var x = "test value"
+
+console.log(x)
+let x = "value"
+
+const myVal = 8;
+myVal = 9;
 const myArr = []
 myArr.push('value')
 console.log(myArr)
